9a7652e659bd2a75179fe1d0ef99486cb96b7506
Tran Anh Cuong <trananhcuong683@gmail.com>
2026-08-14 11:29:01 +0700

    Get the value from input and create new li when hit enter key

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-todo');
+
+  todoList: string[] = [];
+
+  addTodo(value: string) {
+    if(value.trim()) {
+      this.todoList.push(value)
+    }
+  }
+
 }
