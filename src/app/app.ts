a15a40d4cd1fa190f3a7384ac0007241adf306f2
Tran Anh Cuong <trananhcuong683@gmail.com>
2026-08-17 11:25:34 +0700

    Added click function for the check mark on todo item

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -12,12 +13,17 @@ export class App {
   protected readonly title = signal('angular-todo');
 
   todoList: string[] = [];
+  isClick = false;
   HoverIndex: number | null = null;
 
   addTodo(value: string) {
     if(value.trim()) {
       this.todoList.push(value)
     }
+  }
+
+  toggleCheck() {
+    this.isClick = !this.isClick;
   }
 
 }
