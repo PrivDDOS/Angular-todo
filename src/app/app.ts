83a89065957c8a9e57497f3e9db11630cc6acfc5
Tran Anh Cuong <trananhcuong683@gmail.com>
2026-08-18 11:13:24 +0700

    Make checkmark icon when click only toggle on 1 item instead of all item

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgClass } from '@angular/common';
 
+type Todo = {
+  text: string;
+  checked: boolean;
+};
 
 @Component({
   selector: 'app-root',
@@ -11,19 +15,23 @@ import { NgClass } from '@angular/common';
 })
 export class App {
   protected readonly title = signal('angular-todo');
-
-  todoList: string[] = [];
-  isClick = false;
+  
+  
+  todoList: Todo[] = [];
   HoverIndex: number | null = null;
 
   addTodo(value: string) {
     if(value.trim()) {
-      this.todoList.push(value)
+      this.todoList.push({text: value, checked: false})
     }
   }
-
-  toggleCheck() {
-    this.isClick = !this.isClick;
+  
+  toggleCheck(index: number): void {
+    this.todoList[index].checked = !this.todoList[index].checked;
   }
-
+  
+  deleteTodo(index: number): void {
+    this.todoList.splice(index, 1); // remove 1 item at index
+  }
+  
 }
